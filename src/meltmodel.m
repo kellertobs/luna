ae2eca057bb656cda59b4c4396820da0724ef8d0
Tobias Keller <kellertobs@gmail.com>
2022-02-17 16:09:59 +0100

    add code files for melt model calibration

---
+++ src/meltmodel.m
@@ -0,0 +1,235 @@
+function  [phs,cal]  =  meltmodel(var,cal,type)
+
+%*****  compute partition coefficients at P,T *****************************
+
+if strcmp(type,'K')
+    
+    cal  =  K(var.T,var.P,cal);
+    phs  =  [];
+    
+%*****  compute Tsol, Tliq at P,C^i  **************************************
+
+elseif strcmp(type,'T')
+    
+    cal  =  Tsolidus( var,cal);
+    cal  =  Tliquidus(var,cal);
+    phs  =  [];
+
+%*****  compute equilibrium f, C_s^i, C_l^i fraction at P,T,C^i  **********
+
+elseif strcmp(type,'E')
+    
+    phs  =  equilibrium(var,cal);
+    
+end
+
+end
+
+
+function  [cal]  =  Tsolidus(var,cal)
+
+%*****  subroutine to compute solidus temperature at given bulk composition
+
+%***  exclude invalid compositions
+ii   =  sum(var.c(:,sum(var.c,1)>1),2)<=1;
+
+%***  get P-dependent pure-component melting Temperature
+cal  =  K(var.T,var.P,cal);
+
+%***  set starting guess for Tsol
+Tsol =  max(min(cal.Tm(:)),min(max(cal.Tm(:)),sum(var.c.*cal.Tm,2)));
+
+%***  get T-dependent partition coefficients Ki
+cal  =  K(Tsol,var.P,cal);
+
+%***  get residual for sum(ci_b/Ki) = 1
+r    =  sum(var.c./cal.K,2)-1;
+
+rnorm      =  1;     % initialize residual norm for iterations
+n          =  0;     % initialize iteration count
+rnorm_tol  =  1e-12; % tolerance for Newton residual
+its_tol    =  1e3;   % maximum number of iterations
+eps_T      =  0.1;    % temperature perturbation for finite differencing, degrees
+
+while rnorm > rnorm_tol  % iterate down to full accuracy
+    
+    %***  compute partition coefficients Ki at T+eps_T
+    cal   =  K(Tsol+eps_T,var.P,cal);
+    
+    %***  get residual at T+eps_T
+    rp   =  sum(var.c./cal.K,2)-1;
+    
+    %***  compute partition coefficients Ki at T-eps_T
+    cal   =  K(Tsol-eps_T,var.P,cal);
+    
+    %***  get residual at T-eps_T
+    rm   =  sum(var.c./cal.K,2)-1;
+    
+    %***  finite difference drdT = (r(T+eps_T)-r(T-eps_T))/2/eps_T
+    drdT  =  (rp - rm)./2./eps_T;
+    
+    %***  apply Newton correction to current guess of Tsol
+    Tsol(ii)  =  Tsol(ii) - r(ii)./2./drdT(ii);
+    
+    %***  compute partition coefficients Ki at Tsol
+    cal   =  K(Tsol,var.P,cal);
+    
+    %***  compute residual at Tsol
+    r  =  sum(var.c./cal.K,2)-1;
+    
+    %***  compute Newton residual norm
+    rnorm  =  norm(r(ii),2)./sqrt(length(r(ii)));
+    
+    n  =  n+1;   %  update iteration count
+    
+    if (n==its_tol)
+        error(['!!! Newton solver for solidus T has not converged after ',num2str(its_tol),' iterations !!!']);
+    end
+    
+end
+
+cal.Tsol = Tsol;
+
+end
+
+
+function  [cal]  =  Tliquidus(var,cal)
+
+%*****  subroutine to compute liquidus temperature at given bulk composition
+
+%***  exclude invalid compositions
+ii  =  sum(var.c(:,sum(var.c,1)>1),2)<=1;
+
+%***  get P-dependent pure component melting T
+cal  =  K(var.T,var.P,cal);
+
+%***  set starting guess for Tliq
+Tliq  =  max(min(min(cal.Tm)),min(max(max(cal.Tm)),sum(var.c.*cal.Tm,2)));
+
+%***  get T-dependent partition coefficients Ki
+cal  =  K(Tliq,var.P,cal);
+
+%***  get residual for sum(ci_b*Ki) = 1
+r  =  sum(var.c.*cal.K,2)-1;
+
+rnorm      =  1;     % initialize residual norm for iterations
+n          =  0;     % initialize iteration count
+rnorm_tol  =  1e-12; % tolerance for Newton residual
+its_tol    =  1e3;   % maximum number of iterations
+eps_T      =  0.1;   % temperature perturbation for finite differencing, degrees
+
+while rnorm > rnorm_tol  % iterate down to full accuracy
+    
+    %***  compute partition coefficients Ki at T+eps
+    cal  =  K(Tliq+eps_T,var.P,cal);
+    
+    %***  get residual at T+eps_T
+    rp  =  sum(var.c.*cal.K,2)-1;
+    
+    %***  compute partition coefficients Ki at T-eps_T
+    cal  =  K(Tliq-eps_T,var.P,cal);
+    
+    %***  get residual at T-eps_T
+    rm  =  sum(var.c.*cal.K,2)-1;
+    
+    %***  compute difference drdT = (r(T+eps_T)-r(T-eps_T))/2/eps_T
+    drdT  =  (rp - rm)./2./eps_T;
+    
+    %***  apply Newton correction to Tliq
+    Tliq(ii)  =  Tliq(ii) - r(ii)./2./drdT(ii);
+    
+    %***  compute partition coefficients Ki at Tliq
+    cal  =  K(Tliq,var.P,cal);
+    
+    %***  compute residual at Tliq
+    r  =  sum(var.c.*cal.K,2)-1;
+    
+    %***  compute Newton residual norm
+    rnorm  =  norm(r(ii),2)./sqrt(length(r(ii)));
+    
+    n  =  n+1;   %  update iteration count
+    
+    if (n==its_tol)
+        error(['!!! Newton solver for liquidus T has not converged after ',num2str(its_tol),' iterations !!!']);
+    end
+    
+end
+
+cal.Tliq  =  Tliq;
+
+end
+
+
+function  [var]  =  equilibrium(var,cal)
+
+%*****  subroutine to compute equilibrium melt fraction and phase 
+%       compositions at given bulk composition, pressure and temperature
+
+%***  get Tsol, Tliq at c
+cal  =  Tsolidus( var,cal);
+cal  =  Tliquidus(var,cal);
+
+%***  get T,P-dependent partition coefficients Ki
+cal  =  K(max(cal.Tsol,min(cal.Tliq,var.T)),var.P,cal);
+
+%***  set reasonable initial condition if f does not exist or is zero
+if ~isfield(var,'f') 
+    var.f = max(0,min(1,(var.T-cal.Tsol)./(cal.Tliq-cal.Tsol)));
+elseif ~any(var.f(:)>1e-9)
+    var.f = max(0,min(1,(var.T-cal.Tsol)./(cal.Tliq-cal.Tsol)));
+end
+    
+%***  compute residual of unity sum of components
+ff   =  repmat(var.f,1,cal.nc);
+r    =  sum(var.c./(ff+(1-ff).*cal.K),2) - sum(var.c./(ff./cal.K+(1-ff)),2);
+
+rnorm  =  1;       % initialize residual norm for iterations
+n      =  0;       % initialize iteration count
+rnorm_tol = 1e-15; % tolerance for Newton residual
+its_tol   = 500;   % maximum number of iterations
+
+ii = var.T > cal.Tsol & var.T < cal.Tliq;
+
+while rnorm > rnorm_tol     % Newton iteration
+    
+    %***  compute analytic derivative of residual dr/df
+    dr_df  =  - sum(var.c.*(1 -cal.K  )./(ff+(1-ff).*cal.K).^2,2) ...
+              + sum(var.c.*(1./cal.K-1)./(ff./cal.K+(1-ff)).^2,2);
+    
+    %***  apply Newton correction to f
+    var.f(ii) = max(0,min(1,var.f(ii) - r(ii)./2./dr_df(ii)));
+    
+    
+    %***  compute residual of unity sum of components
+    ff     =  repmat(var.f,1,cal.nc);
+    r      =  sum(var.c./(ff+(1-ff).*cal.K),2) - sum(var.c./(ff./cal.K+(1-ff)),2);
+    
+    %***  get non-linear residual norm
+    rnorm  =  norm(r(var.f>0 & var.f<1),2)./sqrt(length(r(var.f>0 & var.f<1))+1);
+    
+    n  =  n+1;  % update iteration count
+    if (n==its_tol)
+        error(['!!! Newton solver for equilibrium f did not converge after ',num2str(its_tol),' iterations !!!']);
+    end
+end
+    
+%***  get C_s^i, C_l^i as functions of K^i, C^i and f, safeguard bounds
+var.cl  =  max(0,min(1, var.c./(ff + (1-ff).*cal.K) ));
+var.cs  =  max(0,min(1, var.c./(ff./cal.K + (1-ff)) ));
+
+end
+
+
+function  [cal]  =  K(T,P,cal)
+
+%***  compute P-dependence of component melting points     
+%     Parameterization as in Rudge etal (2011)
+cal.Tm  =  cal.T0 .* (1 + P./cal.A) .^ (1./cal.B);
+
+%***  compute T,P-dependence of equilibrium partition coefficients
+%     Parameterization after Rudge, Bercovici, & Spiegelman (2010)
+
+cal.L  = (cal.Tm+273.15).*cal.dS;
+cal.K  =  exp(cal.L./cal.r.*(1./(T+273.15) - 1./(cal.Tm+273.15)));
+        
+end % function
---
+++ usr/cal_magma6.m
@@ -0,0 +1,55 @@
+% set number of components
+cal.nc  =  6;
+
+% set string with component names
+cal.CompStr  =  {'fo','fay','opx','cpx','an','ab'};
+for i = 1:cal.nc; cal.(cal.CompStr{i}) = i; end
+
+% set melting model end-member oxide compositions
+%             SiO2  Al2O3    FeO    MgO    CaO   Na2O
+cal.oxds = [  41.5    0.0    3.5   54.9    0.1      0
+              34.5    0.1   44.0   20.8    0.6      0
+              55.0    7.0    4.0   33.0    0.9    0.1
+              48.0    2.0   28.7    5.1   16.0    0.2
+              47.0   33.0    0.5    0.2   18.0    1.3
+              55.0   28.0    1.8    0.2   11.0    4.0  ];
+cal.oxds = cal.oxds./sum(cal.oxds,2)*100;
+
+% specify calibration parameters
+
+% set pure component melting points T_m^i at P=0
+cal.T0(cal.fo )  =  1700;
+cal.T0(cal.fay)  =  1050;
+cal.T0(cal.opx)  =  1300;
+cal.T0(cal.cpx)  =  1000;
+cal.T0(cal.an )  =  1150;
+cal.T0(cal.ab )  =   900;
+
+% set first coeff. for P-dependence of T_m^i [GPa]
+cal.A(cal.fo )   =  11;
+cal.A(cal.fay)   =   6;
+cal.A(cal.opx)   =   4;
+cal.A(cal.cpx)   =   3;
+cal.A(cal.an )   =   8;
+cal.A(cal.ab )   =   4;
+
+% set second coeff. for P-dependence of T_m^i [1]
+cal.B(cal.fo )   =  1.9;
+cal.B(cal.fay)   =  1.7;
+cal.B(cal.opx)   =  1.9;
+cal.B(cal.cpx)   =  1.9;
+cal.B(cal.an )   =  2.1;
+cal.B(cal.ab )   =  2.0;
+
+% set entropy gain of fusion DeltaS [J/K]
+cal.dS           =  300;
+
+% set coeff. for T-dependence of partition coefficients K^i [1/K]
+cal.r(cal.fo )   =  64;
+cal.r(cal.fay)   =  51;
+cal.r(cal.opx)   =  18;
+cal.r(cal.cpx)   =  13;
+cal.r(cal.an )   =   5;
+cal.r(cal.ab )   =  19;
+
+
